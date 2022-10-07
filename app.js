11239856f683bb32e38d7946dec868bb5c268779
TinaLacatis <danatina.dt@gmail.com>
2022-10-07 18:31:43 +0200

    update app.js

--- app.js
+++ app.js
@@ -64,11 +64,6 @@ app.route("/articles/:articleTitle")
       } else {
         res.send("No articles matching that title was found.");
       }
-      // if(!err){
-      //   res.send(foundArticles);
-      // } else {
-      //   res.send(err);
-      // }
     });
   })
 
@@ -113,32 +108,3 @@ app.route("/articles/:articleTitle")
 app.listen(3000, function() {
   console.log("Server started succesfully");
 });
-
-
-
-
-// {
-//     "_id" : "63402fa123073c2b87228149",
-//     "title" : "REST",
-//     "content" : "REST is short for REpresentational State Transfer. It's an architectural style for designing APIs"
-// }
-// {
-//     "_id" : "634030c923073c2b8722814a",
-//     "title" : "API",
-//     "content" : "API stands for Application Programming Interface. It is a set of subroutine definitions, communication protocols, and tools for building software. In general terms, it is a set of clearly defined methods of communication among various components. A good API makes it easier to develop a computer program by providing all the building blocks, which are then put together by the programmer."
-// }
-// {
-//     "_id" : "634030d623073c2b8722814b",
-//     "title" : "Bootstrap",
-//     "content" : "This is a framework developed by Twitter that contains pre-made front-end templates for web design"
-// }
-// {
-//     "_id" : "634030e023073c2b8722814c",
-//     "title" : "DOM",
-//     "content" : "The Document Object Model is like an API for interacting with our HTML"
-// }
-// {
-//     "_id" : "63403dc5bed948eec93fb5d0",
-//     "title" : "Jack Bauer",
-//     "content" : "Jack Bauer is able to build a snowman out of water.",
-// }
